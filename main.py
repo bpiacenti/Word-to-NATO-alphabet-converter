680436b341425572c7473696aa431f00af0d8cf1
devbenpiacenti <devbenpiacenti@gmail.com>
2025-04-10 18:28:19 -0400

    update to catch KeyError in user input

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import pandas
 
 nato_df = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-word_input = input("Enter a word: ").upper()
-# print(word_input)
+def nato_conversion():
+    word_input = input("Enter a word: ").upper()
+    # print(word_input)
+    try:
+        nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
+        # print(nato_dict)
+        word_coded = [nato_dict[letter] for letter in word_input]
+    except KeyError:
+        print("Please use only letters in the alphabet.")
+        nato_conversion()
+    else:
+        print(word_coded)
 
-nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
-# print(nato_dict)
-
-word_coded = [nato_dict[letter] for letter in word_input]
-print(word_coded)
+nato_conversion()
